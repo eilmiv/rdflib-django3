c87fdd5d71cff7b0fcb5088d4314fa20f0947c14
Martin Voigt <m.voigt@hzdr.de>
2023-11-01 10:41:05 +0100

    Test conjunctive graphs

--- test/test_store.py
+++ test/test_store.py
@@ -6,9 +6,11 @@ import datetime
 
 import rdflib
 from django import test
-from rdflib.graph import Graph
+from rdflib.graph import Graph, ConjunctiveGraph
 from rdflib.namespace import RDF, RDFS, Namespace
 from rdflib.term import BNode, Literal, URIRef
+
+from rdflib_django.store import DjangoStore
 
 EX = Namespace("http://www.example.com/")
 
@@ -111,3 +113,30 @@ class GraphTest(test.TestCase):
         self.assertEqual(
             list(self.graph), [(artis, EX["prop"], Literal(False))]
         )
+
+    def testConjunctiveGraph(self):
+        ex = Namespace("https://www.example.org/")
+        store1 = DjangoStore(identifier="store1")
+        store2 = DjangoStore(identifier="store2")
+
+        g1 = Graph(store1, identifier=ex.g1)
+        g2 = ConjunctiveGraph(store1, identifier=ex.g2)
+        g3 = Graph(store2, identifier=ex.g3)
+
+        g1.add((ex.a, ex.a, ex.a))
+        g2.add((ex.b, ex.b, ex.b))
+        g3.add((ex.c, ex.c, ex.c))
+
+        self.assertEquals(len(g1), 1)
+        self.assertEquals(len(g2), 2)
+        self.assertEquals(len(g3), 1)
+
+        self.assertEquals(set(g1), {(ex.a, ex.a, ex.a)})
+        self.assertEquals(set(g2), {(ex.a, ex.a, ex.a), (ex.b, ex.b, ex.b)})
+        self.assertEquals(set(g3), {(ex.c, ex.c, ex.c)})
+
+        g2.remove((None, None, None))
+
+        self.assertEquals(list(g1), [])
+        self.assertEquals(list(g2), [])
+        self.assertEquals(list(g3), [(ex.c, ex.c, ex.c)])
